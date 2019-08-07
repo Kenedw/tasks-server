29f1ef02efec29fc18bd1d65c5c68315c3a687df
KenedW <kenedwanderson@live.com>
2019-08-07 20:04:14 -0300

    initial config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    node: true
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "semistandard"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {
+    indent: "off",
+    "@typescript-eslint/indent": ["error", 2]
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const world = '🗺️';
+
+export function hello (word: string = world): string {
+  return `Hello ${word}! `;
+}
+
+console.log(hello());
